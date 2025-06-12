00b2f93cc335e4b87e57a7177fab5ed4544ee8c3
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-12 09:30:18 +0530

    intro py step 1

---
+++ intro.py
@@ -0,0 +1,11 @@
+#milage converter
+print("how many kilometer did you cycle today")
+kms = input()
+miles = float(kms)/1.60943
+miles = round(miles,2)
+print(f"your {kms} km to {miles} miles")
+
+#------------- ------------ -------------------
+
+
+
