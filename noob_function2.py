e7d464acd18705f112f4a98ac04be3c889dd7d69
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-12 23:13:37 +0530

    function define2 without using args

---
+++ noob_function2.py
@@ -0,0 +1,7 @@
+# ----function part2--------
+def sum_all_nums(num1,num2,num3,num4,num5):
+    return num1+num2+num3+num4+num5
+print(sum_all_nums(4,6,8.7,9,3))
+print(sum_all_nums(3,5,2,56,78))
+# using args -------------- doing above easily
+
