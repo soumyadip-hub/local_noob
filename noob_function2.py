15c563f5caa349e25cdab533e8e0ab1768108f70
soumyadip_pakhira <130168663+soumyadip-hub@users.noreply.github.com>
2025-06-12 23:42:24 +0530

    Update noob_function2.py

--- noob_function2.py
+++ noob_function2.py
@@ -3,7 +3,7 @@ def sum_all_nums(num1,num2,num3,num4,num5):
     return num1+num2+num3+num4+num5
 print(sum_all_nums(4,6,8.7,9,3))
 print(sum_all_nums(3,5,2,56,78))
-using args -------------- doing above easily
+#using args -------------- doing above easily
 def sum_all_nums(*args):
     total=0
     for num in args:
