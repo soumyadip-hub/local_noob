869cf48ca080a2ca8980775e8ed897f6a71a0174
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-12 23:18:00 +0530

    no change

--- noob_function2.py
+++ noob_function2.py
@@ -4,4 +4,10 @@ def sum_all_nums(num1,num2,num3,num4,num5):
 print(sum_all_nums(4,6,8.7,9,3))
 print(sum_all_nums(3,5,2,56,78))
 # using args -------------- doing above easily
-
+def sum_all_nums(*args):
+    total=0
+    for num in args:
+        total += num
+    return total
+print(sum_all_nums(4,6,7,89,3))
+print(sum_all_nums(4,6))
