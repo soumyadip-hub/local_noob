8b7806898ade6cb31ee3aea8ea186ec214d3a5f9
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-12 22:29:28 +0530

    function define2

--- noob_function.py
+++ noob_function.py
@@ -8,3 +8,24 @@ def sing_happy_birthday():
 sing_happy_birthday()
 sing_happy_birthday()
 sing_happy_birthday()
+
+#-----------coin_flip function-----
+
+from random import random
+
+def flip_coin():
+    #genearte random number between 0-1
+    r=random()
+    if r > 0.5:
+        return"Heads"
+    else:
+        return "Tails"
+ 
+print(flip_coin())
+
+#using parameters------
+
+def square(num):
+    return num * num
+print(square(4))
+print(square(5))
