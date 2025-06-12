f4fc775324f735a470924d7e025d8d13339a9ba1
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-12 21:30:51 +0530

    function define

---
+++ noob_function.py
@@ -0,0 +1,10 @@
+# ---------FUNCTION-------
+def sing_happy_birthday():
+    print("happy birthday to you")
+    print("happy birthday to you")
+    print("happy birthday dear to you")
+    print("Happy birthday To You")
+
+sing_happy_birthday()
+sing_happy_birthday()
+sing_happy_birthday()
