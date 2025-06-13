6a3cda0ee0bf2901f70e9a43f263136b51464827
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-13 09:47:02 +0530

    little modules

---
+++ noob_modules.py
@@ -0,0 +1,10 @@
+# ------------here we see modules which already built in--------
+import random
+
+print(random.choice(["apple","banana","cherry","mango"]))
+print(random.randint(1,100))
+# -----------
+import random as r
+
+print(r.choice(["apple","banana","mango","jackfruit"]))
+# ------
