55daa2b917c17ddad2a0d698ffde53f06b1c5d33
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-13 08:09:57 +0530

    filter function first examples in lamdas function

--- noob_lambdas.py
+++ noob_lambdas.py
@@ -1,3 +1,4 @@
+# built -in - functions
 # ------using map function(using lambdas)-----
 nums = [2,4,6,89,1]
 doubles = map(lambda x: x*2, nums)
@@ -10,4 +11,9 @@ people = ["abhishek","nobin","indranil","mir","sourendu"]
 peeps = map(lambda name: name.upper(), people)
 
 print(list(peeps))
+
+#------filter function-----------
+names = ["austin","penny","augusta","sombu","aritra"]
+a_names = filter(lambda n: n[0]=='a', names)
+print(list(a_names))
 
