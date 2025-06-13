ec8f4bd31a4190fe423d2aee795e4c96d99bf49b
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-13 18:56:10 +0530

    learn what is class

---
+++ oops_one.py
@@ -0,0 +1,15 @@
+# ----oops tell avout class--------
+class user:
+    pass
+
+user1 = user()
+user2 = user()
+user3 = user()
+user4 = user()
+print(user1)
+print(user2)
+print(user3)
+
+# --------tell about gives attribute to class-----
+
+
