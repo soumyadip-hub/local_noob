f2fb396ab045fa64652ad88788401db61bebd3fc
noobtolearner <soumyadippakhira094@gmail.com>
2025-06-14 15:55:19 +0530

    learn about constructor

--- oops_one.py
+++ oops_one.py
@@ -16,3 +16,27 @@ print(e2.get_salary())
 print(e2.company)
 
 
+# ----------------------------------------
+
+
+# -----constructors- use constructors to initialize our objects-----
+
+class Employee:
+
+    def __init__(self, salary, name, bond):
+        self.salary = salary #creat an instance attribute of name salary and assignit with it salary
+        self.name = name
+        self.bond = bond
+
+    def get_salary(self): #self is important here because self a way to reference the object of the class which is ebing created
+        return self.salary
+    
+    def get_info(self):
+        print(f"the name of the employee is {self.name}. salary is {self.salary}.the bond is for {self.bond}years")
+    
+e1 = Employee(34000,"john doe",4)
+# print(e1.get_salary())
+
+e1.get_info()
+
+# ---------
